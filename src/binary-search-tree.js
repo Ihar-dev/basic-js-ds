f512e41f8b75165de83f6c5a21380ab54d9d8a3f
Ihar Kozik <ihar.kozik@gmail.com>
2021-10-17 21:32:42 +0300

    feat: add add(data) method

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,34 +1,58 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {
+  NotImplementedError
+} = require('../extensions/index.js');
 
 // const { Node } = require('../extensions/list-tree.js');
 
 /**
-* Implement simple binary search tree according to task description
-* using Node from extensions
-*/
+ * Implement simple binary search tree according to task description
+ * using Node from extensions
+ */
 module.exports = class BinarySearchTree {
 
+  constructor() {
+    this.root1 = null;
+  }
   root() {
+    return this.root1;
+  }
+
+  add(data) {
+    const newNode = new Node(data);
+    if (!this.root1) {
+      this.root1 = newNode;
+      return;
+    }
+
+    let currentNode = this.root1;
+    while (currentNode) {
+      if (newNode.data < currentNode.data) {
+        if (!currentNode.left) {
+          currentNode.left = newNode;
+          return;
+        }
+        currentNode = currentNode.left;
+      } else {
+        if (!currentNode.right) {
+          currentNode.right = newNode;
+          return;
+        }
+        currentNode = currentNode.right;
+      }
+    }
+  }
+
+  has( /* data */ ) {
     throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
 
-  add(/* data */) {
+  find( /* data */ ) {
     throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-
-  remove(/* data */) {
+  remove( /* data */ ) {
     throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
@@ -43,4 +67,11 @@ module.exports = class BinarySearchTree {
     // remove line with error and write your code here
   }
 
+}
+class Node {
+  constructor(data, left = null, right = null) {
+    this.data = data;
+    this.left = left;
+    this.right = right;
+  }
 }
